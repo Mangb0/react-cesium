c0bd0f7f9f5ae9c244fe9eee4b965a81a1c3171c
Mangbo <whdudghks45629@gmail.com>
2023-09-06 16:29:09 +0900

    Displaying an entity

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-import { Viewer } from "resium";
+import { Viewer, Entity, PointGraphics } from "resium";
+import { Cartesian3 } from "cesium";
+
+const position = Cartesian3.fromDegrees(-74.0707383, 40.7117244, 100);
 
 function App() {
-  return <Viewer />;
+  return (
+    <Viewer full>
+      <Entity position={position}>
+        <PointGraphics pixelSize={10} />
+      </Entity>
+    </Viewer>
+  );
 }
 
 export default App;
