2d364909cf56931acdca9056b51a8e72795b0afb
Mangbo <whdudghks45629@gmail.com>
2023-09-06 14:05:34 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+import { Viewer } from "resium";
+
+function App() {
+  return <Viewer />;
+}
+
+export default App;
