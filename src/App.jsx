43eb02e888cc8853af61dcdc6d1d38b803eb52d6
whdudghks456 <whdudghks45629@gmail.com>
2023-09-08 09:33:17 +0900

    Add IonResourcePreview

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { Route, Routes, Link, useLocation } from "react-router-dom";
 import EntityPreview from "./pages/EntityPreview";
 import Home from "./pages/Home";
+import IonResourcePreview from "./pages/IonResourcePreview.jsx";
 
 function App() {
-  const pageArray = ["/entity"];
+  const pageArray = ["/entity", "/ion"];
   const location = useLocation();
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/entity" element={<EntityPreview />} />
+        <Route path="/ion" element={<IonResourcePreview />} />
       </Routes>
       <ul>
         {pageArray.map((item, index) => {
